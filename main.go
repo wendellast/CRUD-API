1784e032cfef2eb8ca10ce519c50896a1a1c7db4
wendellast <wendelalves01a@hotmail.com>
2024-08-22 06:57:09 -0700

    routes

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/wendellast/CRUD-API/src/controller/routes"
 )
 
 func main() {
@@ -12,4 +14,10 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	router := gin.Default()
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ src/controller/routes/routes.go
@@ -0,0 +1,16 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/wendellast/CRUD-API/src/controller"
+)
+
+func InitRoutes(r *gin.RouterGroup) {
+
+	r.GET("/getUserById/:userId", controller.FindUserByID)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createUser", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
+
+}
