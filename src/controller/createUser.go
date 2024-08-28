91f5fe3a1c3809e4e65d389808405559289f6f71
wendellast <wendelalves01a@hotmail.com>
2024-08-28 10:25:25 -0700

    feat: add looger

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,19 +1,22 @@
 package controller
 
 import (
-	"fmt"
-	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/wendellast/CRUD-API/src/configuration/logger"
 	"github.com/wendellast/CRUD-API/src/configuration/validation"
 	"github.com/wendellast/CRUD-API/src/controller/model/request"
+	"github.com/wendellast/CRUD-API/src/controller/model/response"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser Controller", zap.String("journey", "createuser"))
 	var userResquest request.UserResquest
 
 	if err := c.ShouldBindJSON(&userResquest); err != nil {
-		log.Printf("Error trying to marshal object, error %s ", err.Error())
+		logger.Error("Error trying to validate user info ", err)
 		errRest := validation.ValidateUserError(err)
 
 		c.JSON(errRest.Code, errRest)
@@ -21,6 +24,14 @@ func CreateUser(c *gin.Context) {
 		return
 	}
 
-	fmt.Print(userResquest)
+	response := response.UseResponse{
+		Id:    "test",
+		Email: userResquest.Email,
+		Name:  userResquest.Name,
+		Age:   userResquest.Age,
+	}
 
+	logger.Info("User created successfully", zap.String("journey", "createuser"))
+
+	c.JSON(http.StatusOK, response)
 }
